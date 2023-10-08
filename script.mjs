b6701c93caa974e1d09802e55eaf0774e7f82aeb
Abdur Rahman <111abdurrehman111@gmail.com>
2023-10-08 17:49:19 +0500

    java variables

--- script.mjs
+++ script.mjs
@@ -1,1 +1,13 @@
 console.log("hello world from mjs file")
+
+let myNumber = 7;
+let myName = "Abdur Rahman"
+
+console.log(myNumber, myName)
+
+
+myNumber = 40
+myName = "Abdur Rahman bin Shoaib"
+
+console.log(myNumber, myName)
+
