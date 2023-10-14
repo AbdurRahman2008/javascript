de7e4f38f9aebc4ff9f6d184b343b55b3d2b1d8d
Abdur Rahman <111abdurrehman111@gmail.com>
2023-10-14 16:28:53 +0500

    java

--- script.mjs
+++ script.mjs
@@ -1,13 +1,15 @@
 console.log("hello world from mjs file")
 
 let myNumber = 7;
-let myName = "Abdur Rahman"
+let myName = "Abdur Rahman";
 
-console.log(myNumber, myName)
+console.log(myNumber)
+console.log( myName)
 
 
-myNumber = 40
-myName = "Abdur Rahman bin Shoaib"
+myNumber = 40;
+myName = "Abdur Rahman bin Shoaib";
 
-console.log(myNumber, myName)
+console.log(myNumber)
+console.log( myName)
 
